bbb2e1152b52306990f0bf8a2cacee7ed0093ba8
Noah McMillan <noahm27@gmail.com>
2023-08-03 23:35:27 -0700

    Inital commit

---
+++ fetcher.js
@@ -0,0 +1,44 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const getHTML = (arg) => {
+  let url = arg[2];
+  let file = arg[3];
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log("Error:", error);
+    }
+    // checks if the file exists and asks to overwrite if it does
+    if (fs.existsSync(file)) {
+      rl.question('File already exists. Would you like to overwrite it? Y/N ', (answer) => {
+        if (answer === 'Y') {
+          writeToFile(file, body);
+        }
+      });
+    } else {
+      writeToFile(file, body);
+    }
+  });
+  rl.close();
+};
+
+const writeToFile = (file, content) => {
+  fs.writeFile(file, content, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Downloaded and saved ${content.length} bytes to ${file}`);
+    }
+  });
+};
+
+
+
+getHTML(process.argv);
